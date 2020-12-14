d6cfa701f9709ad156769314bf62b3b2230e9707
Bilal Hafri <hafribilal@gmail.com>
2020-12-14 23:59:01 +0100

    Second Day -Complete :  +Fragment Payment  +Fragment Reload Subscription linked with Payment  +Fragment Bills Payment linked with Payment

--- app/src/main/java/ma/onee/electrique/ui/billsPayment/BillsPaymentFragment.java
+++ app/src/main/java/ma/onee/electrique/ui/billsPayment/BillsPaymentFragment.java
@@ -4,32 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import ma.onee.electrique.PaymentFragment;
 import ma.onee.electrique.R;
 
 public class BillsPaymentFragment extends Fragment {
 
     private BillsPaymentViewModel billsPaymentViewModel;
+    Button btn_Next2PaymentFromBillsPay;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         billsPaymentViewModel =
                 ViewModelProviders.of(this).get(BillsPaymentViewModel.class);
         View root = inflater.inflate(R.layout.fragment_bills_payment, container, false);
-//        final TextView textView = root.findViewById(R.id.text_gallery);
-//        billsPaymentViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+        btn_Next2PaymentFromBillsPay = root.findViewById(R.id.btn_Next2PaymentFromBillsPay);
+
+        btn_Next2PaymentFromBillsPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new PaymentFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_bills_payment, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+//        
         return root;
     }
 }
--- app/src/main/java/ma/onee/electrique/ui/reloadSubscription/ReloadSubscriptionFragment.java
+++ app/src/main/java/ma/onee/electrique/ui/reloadSubscription/ReloadSubscriptionFragment.java
@@ -1,5 +1,7 @@
 package ma.onee.electrique.ui.reloadSubscription;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -12,19 +14,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import ma.onee.electrique.PaymentFragment;
 import ma.onee.electrique.R;
 
 public class ReloadSubscriptionFragment extends Fragment {
 
     private ReloadSubscriptionViewModel reloadSubscriptionViewModel;
+    Button btn_Next2PaymentFromReloadSubs;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         reloadSubscriptionViewModel =
                 ViewModelProviders.of(this).get(ReloadSubscriptionViewModel.class);
         View root = inflater.inflate(R.layout.fragment_reload_subscription, container, false);
+        btn_Next2PaymentFromReloadSubs = root.findViewById(R.id.btn_Next2PaymentFromReloadSubs);
 //        final TextView textView = root.findViewById(R.id.text_gallery);
 //        reloadSubscriptionViewModel.getText().observe(this, new Observer<String>() {
 //            @Override
@@ -33,6 +39,17 @@ public class ReloadSubscriptionFragment extends Fragment {
 //            }
 //        });
 
+        btn_Next2PaymentFromReloadSubs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new PaymentFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_reload_subscription, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
        return root;
     }
 
